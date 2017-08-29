ca87fc242b42d1a8f924b03b3b38029e0804fec5
zibs <elizibin@gmail.com>
2017-08-28 21:56:16 -0700

    begin issue two work

--- app/controllers/essays_controller.rb
+++ app/controllers/essays_controller.rb
@@ -1,20 +1,53 @@
 class EssaysController < ApplicationController
-  
+
   def no_studio_no_privilege
     @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'no_studio_no_privilege.md'))
+    render_show
   end
 
   def my_argument_is_wrong_but
     @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'my_argument_is_wrong_but.md'))
+    render_show
   end
 
   def how_to_tame_a_fly
     @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'how_to_tame_a_fly.md'))
+    render_show
+  end
+
+  def the_empty_husk_condition
+    @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'the_empty_husk_condition.md'))
+    render_show
+  end
+
+  def adventures_from_the_third_bureau
+    @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'adventures_from_the_third_bureau.md'))
+    render_show
+  end
+
+  def the_bus_as_hostile_architecture_I
+    @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'the_bus_as_hostile_architecture_I.md'))
+    render_show
+  end
+
+  def ciff_selections
+    @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'ciff_selections.md'))
+    render_show
+  end
+
+  def misogyny_as_orientation
+    @markdown_content = File.read(File.join(Rails.root.to_s, 'app/views/essays', 'misogyny_as_orientation.md'))
+    render_show
   end
 
   def index
 
   end
 
+
+  private
+    def render_show
+      render "show"
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get "No-Studio-No-Privilege" => "essays#no_studio_no_privilege", as: :no_studio
   get "My-Argument-Is-Wrong-But" => "essays#my_argument_is_wrong_but", as: :wrong_argument
   get "How-to-Tame-a-Fly" => "essays#how_to_tame_a_fly", as: :tame_fly
+  get "The-Empty-Husk-Condition" => "essays#the_empty_husk_condition", as: :empty_husk
+  get "Adventures-from-the-Third-Bureau" => "essays#adventures_from_the_third_bureau", as: :third_bureau
+  get "The-Bus-as-Hostile-Architecture" => "essays#the_bus_as_hostile_architecture_I", as: :bus_I
+  get "CIFF-Official-Selection-Reviews" => "essays#ciff_selections", as: :ciff_selections
+  get "Misogyny-as-Orientation" => "essays#misogyny_as_orientation", as: :mis_o
 end
